87c7ca11e568e8ac917ccb80d272ae2219005e07
dzyker <grif.master80@gmail.com>
2026-03-28 19:25:41 +0300

    karuzo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Dzykerr!!!!!</h1>')
+  res.send('<h1>Hello Karuzo!!!</h1>')
 })
 
 const PORT = 8080
